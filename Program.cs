e100233126932a6b93e7244e206248bff3db3900
701030552 <701030552@2260I-L29-031>
2025-02-10 14:42:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3_console_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string year;
+          
+           Console.WriteLine("What's your name?");
+           name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+            Console.WriteLine("Your age?");
+
+            age = Console.ReadLine();
+            Console.WriteLine($"Cool, you're {age} years old.");
+
+           
+            Console.WriteLine($"What is the current year?");
+                 Console.WriteLine(DateTime.Now.Year);
+            year = Console.ReadLine();
+         Console.WriteLine($"Oh it is {DateTime.Now.Year}.");
+            
+        }
+    }
+}
